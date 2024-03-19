5fefb785e5e7fa793f9f1106de695b91bf9aa8a1
Vinil Kappa ChandraBabu <156454928+vinil2405@users.noreply.github.com>
2024-03-18 23:10:59 -0500

    bonus

--- routes/computation.js
+++ routes/computation.js
@@ -4,11 +4,10 @@ const a = Math.random();
 const b = Math.random();
 router.get('/', function(req, res) {
     const difference = Math.abs(a - b);
-    res.send('Math.abs(a - b) applied to ${a} and ${b} is ${difference}');
+    res.send(`Math.abs(a - b) applied to ${a} and ${b} is ${difference}`);
 });
 router.get('/', function(req, res) {
     res.send('Respond with a resource');
 });
 
 module.exports = router;
-npm 
